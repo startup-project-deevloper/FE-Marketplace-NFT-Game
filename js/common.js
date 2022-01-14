ce24af10674f6a5191da79bc787bd7bbf4a9595b
CHHuy <chehoanghuy2015@gmail.com>
2022-01-14 17:59:05 +0700

    update box-launch.html, inventory.html popover coming soon fix js error log

--- js/common.js
+++ js/common.js
@@ -89,41 +89,39 @@ if (Modernizr.touch === true && window.innerWidth <= 767) {
       }
     }
 
-    const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+    const deadline = 'January 24 2022 10:00:00 UTC+7';
+    // const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
     initializeClock('countDown', deadline);
-
   }
 
-  /*  function countDownTimer() {
-      function getTimeRemaining(endTime) {
-        const total = Date.parse(endTime) - Date.parse(new Date());
-        const seconds = Math.floor((total / 1000) % 60);
-        const minutes = Math.floor((total / 1000 / 60) % 60);
-        const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-        const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  function countDownLaunch() {
+    function getTimeRemaining(endtime) {
+      const total = Date.parse(endtime) - Date.parse(new Date());
+      const seconds = Math.floor((total / 1000) % 60);
+      const minutes = Math.floor((total / 1000 / 60) % 60);
+      const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
 
-        return {
-          total,
-          days,
-          hours,
-          minutes,
-          seconds
-        };
-      }
+      return {
+        total,
+        hours,
+        minutes,
+        seconds
+      };
+    }
 
-      function initializeClock(endTime) {
-        const daysSpan = $('.days');
-        const hoursSpan = $('.hours');
-        const minutesSpan = $('.minutes');
-        const secondsSpan = $('.seconds');
+    function initializeClock(id, endTime) {
+      if (document.getElementById(id)) {
+        const clock = document.getElementById(id);
+        const hoursSpan = clock.querySelector('.hours');
+        const minutesSpan = clock.querySelector('.minutes');
+        const secondsSpan = clock.querySelector('.seconds');
 
         function updateClock() {
           const t = getTimeRemaining(endTime);
 
-          daysSpan.html(t.days);
-          hoursSpan.html(('0' + t.hours).slice(-2))
-          minutesSpan.html(('0' + t.minutes).slice(-2))
-          secondsSpan.html(('0' + t.seconds).slice(-2))
+          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
           if (t.total <= 0) {
             clearInterval(timeInterval);
@@ -133,19 +131,55 @@ if (Modernizr.touch === true && window.innerWidth <= 767) {
         updateClock();
         const timeInterval = setInterval(updateClock, 1000);
       }
+    }
 
-      const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+/*    const schedule = [
+      ['January 24 2022 10:00:00 UTC+7', 'January 25 2022 10:00:00 UTC+7']
+    ];
 
+    for (var i = 0; i < schedule.length; i++) {
+      var startDate = schedule[i][0];
+      var endDate = schedule[i][1];
 
-      initializeClock(deadline);
+      // put dates in milliseconds for easy comparisons
+      var startMs = Date.parse(startDate);
+      var endMs = Date.parse(endDate);
+      var currentMs = Date.parse(new Date());
 
-    }*/
+      // if current date is between start and end dates, display clock
+      if (endMs > currentMs && currentMs >= startMs) {
+        initializeClock('countDownLaunch', endDate);
+      }
+    }
 
+    schedule.forEach(([startDate, endDate]) => {
+      // put dates in milliseconds for easy comparisons
+      const startMs = Date.parse(startDate);
+      const endMs = Date.parse(endDate);
+      const currentMs = Date.parse(new Date());
+
+      // if current date is between start and end dates, display clock
+      if (endMs > currentMs && currentMs >= startMs) {
+        initializeClock('countDownLaunch', endDate);
+      }
+    });*/
+    const deadlineLaunch = 'January 24 2022 10:00:00 UTC+7';
+    initializeClock('countDownLaunch', deadlineLaunch);
+  }
+
+  function bsPopover(){
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+  }
 
   function init() {
     getBarwidth()
     clearFilter()
     countDownTimer()
+    countDownLaunch()
+    bsPopover()
   }
 
   $(document).ready(function () {
